31a9705218414b39ac16b24bfbf21495b6986bb9
madhavikhatawakar <madhavikh@cybage.com>
2015-11-24 18:58:40 +0530

    Express port 3003

--- server.js
+++ server.js
@@ -25,5 +25,5 @@ app.set('port', process.env.PORT || 3003);
 
  
 var server = app.listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + server.address().port);
+  console.log('Express port ' + server.address().port);
 });
